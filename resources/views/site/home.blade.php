23483fed813d5abd51d322ce6f03f57b26605670
Gabriel_Cunha <gabrielvlog11@gmail.com>
2023-08-10 12:12:57 -0300

    feat: "add homepage"

---
+++ resources/views/site/home.blade.php
@@ -0,0 +1,27 @@
+@extends('site.layout')
+@section('conteudo')
+
+
+
+<div class="row container">
+    @foreach ($events as $event)
+        <div class="col s12 m4">
+            <div class="card">
+                <div class="card-image">
+                <img src="{{ $event->image }}">
+                <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+                </div>
+                <div class="card-content">
+                <span class="card-title">{{ $event->name }}</span>
+                <p>{{ Str::limit($event->description, 20)}}</p>
+                </div>
+            </div>
+        </div>
+    @endforeach
+</div>
+
+<div class="row">
+    {{ $events->links('custom.pagination') }}
+</div>
+
+@endsection
